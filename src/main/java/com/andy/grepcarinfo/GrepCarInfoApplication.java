7352be8891d72aedb7527a62526e56347e1e6cfe
andychen <andy.chen@iisigroup.com>
2021-02-20 16:13:18 +0800

    spring init

---
+++ src/main/java/com/andy/grepcarinfo/GrepCarInfoApplication.java
@@ -0,0 +1,13 @@
+package com.andy.grepcarinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrepCarInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrepCarInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andy/grepcarinfo/GrepCarInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andy.grepcarinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrepCarInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
