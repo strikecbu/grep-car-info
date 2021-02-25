639c06f2790d4153ac1fd9d2f530006c01376f47
andychen <andy.chen@iisigroup.com>
2021-02-26 01:40:30 +0800

    change port and MariaDB user

--- src/test/java/com/andy/grepcarinfo/GrepCarInfoApplicationTests.java
+++ src/test/java/com/andy/grepcarinfo/GrepCarInfoApplicationTests.java
@@ -2,8 +2,10 @@ package com.andy.grepcarinfo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles({"default", "mariadb"})
 class GrepCarInfoApplicationTests {
 
     @Test
