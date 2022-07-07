44447aac82d376a334d165d0e90cb0c668688a31
strikecbu <strikecbu@gmail.com>
2022-07-07 10:20:09 +0800

    [Scrap] Add dependencies for config server and config settings

--- car-config/src/main/java/com/andysrv/carconfig/CarConfigApplication.java
+++ car-config/src/main/java/com/andysrv/carconfig/CarConfigApplication.java
@@ -2,8 +2,10 @@ package com.andysrv.carconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class CarConfigApplication {
 
     public static void main(String[] args) {
