4ec420b3797f4bb239a7ac26127676b68083f9be
strikecbu <strikecbu@gmail.com>
2022-07-07 10:23:24 +0800

    [Eureka] Init project resources

---
+++ car-eureka/src/main/java/com/example/careureka/CarEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.example.careureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarEurekaApplication.class, args);
+    }
+
+}
