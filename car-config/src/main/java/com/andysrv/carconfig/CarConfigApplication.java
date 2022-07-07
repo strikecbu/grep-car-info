4d6a6b43de42e5107042ef2d41d16370e31b73c1
strikecbu <strikecbu@gmail.com>
2022-07-07 09:40:41 +0800

    [Config] Add init resource

---
+++ car-config/src/main/java/com/andysrv/carconfig/CarConfigApplication.java
@@ -0,0 +1,13 @@
+package com.andysrv.carconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarConfigApplication.class, args);
+    }
+
+}
---
+++ car-config/src/test/java/com/andysrv/carconfig/CarConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andysrv.carconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
