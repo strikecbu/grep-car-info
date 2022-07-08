86c1c1a302e4362658efed55cf9d148c98404500
strikecbu <strikecbu@gmail.com>
2022-07-08 09:24:22 +0800

    [Gateway] Add gateway service resources

---
+++ car-gateway/src/main/java/com/andysrv/cargateway/CarGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.andysrv.cargateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarGatewayApplication.class, args);
+    }
+
+}
---
+++ car-gateway/src/test/java/com/andysrv/cargateway/CarGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andysrv.cargateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
