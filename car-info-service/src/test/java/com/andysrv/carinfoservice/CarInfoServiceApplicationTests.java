0ae3da4961d943bab2829c6fd297bcc7924ae173
strikecbu <strikecbu@gmail.com>
2022-07-15 14:39:23 +0800

    [Scrape] Add carInfoService resources

---
+++ car-info-service/src/test/java/com/andysrv/carinfoservice/CarInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andysrv.carinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarInfoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
