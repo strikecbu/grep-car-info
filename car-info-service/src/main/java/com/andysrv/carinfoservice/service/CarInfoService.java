379b9099aaa9654750eba471025576b7e9ef64cd
strikecbu <strikecbu@gmail.com>
2022-07-15 17:30:40 +0800

    [Scrape] Implement save and getAll CarInfo service

---
+++ car-info-service/src/main/java/com/andysrv/carinfoservice/service/CarInfoService.java
@@ -0,0 +1,70 @@
+package com.andysrv.carinfoservice.service;
+
+import com.andysrv.carinfoservice.entity.CarInfo;
+import com.andysrv.carinfoservice.entity.CarPrice;
+import com.andysrv.carinfoservice.repository.CarInfoRepository;
+import com.andysrv.carinfoservice.repository.CarPriceRepository;
+import org.reactivestreams.Publisher;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CarInfoService {
+
+    private final CarInfoRepository infoRepository;
+    private final CarPriceRepository priceRepository;
+
+    public CarInfoService(CarInfoRepository carInfoRepository, CarPriceRepository carPriceRepository) {
+        this.infoRepository = carInfoRepository;
+        this.priceRepository = carPriceRepository;
+    }
+
+    public Mono<CarInfo> saveOrUpdate(Publisher<CarInfo> publisher) {
+        return Mono.from(publisher)
+                .flatMap(carInfo -> {
+                    return infoRepository.findByTitleAndYear(carInfo.getTitle(), carInfo.getYear())
+                            .defaultIfEmpty(carInfo)
+                            .flatMap(updateCar -> {
+                                CarPrice price = CarPrice.builder()
+                                        .price(carInfo.getPrice())
+                                        .createTime(LocalDateTime.now())
+                                        .build();
+                                return priceRepository.save(price)
+                                        .flatMap(carPrice -> {
+                                            if (updateCar.getId() != null) {
+                                                updateCar.setUpdateTime(LocalDateTime.now());
+                                                updateCar.getPriceIds()
+                                                        .add(carPrice.getId());
+                                                return infoRepository.save(updateCar);
+                                            } else {
+                                                carInfo.setCreateTime(LocalDateTime.now());
+                                                carInfo.setUpdateTime(LocalDateTime.now());
+                                                carInfo.setPriceIds(List.of(carPrice.getId()));
+                                                return infoRepository.save(carInfo);
+                                            }
+                                        });
+
+                            });
+                });
+    }
+
+    public Flux<CarInfo> getAllCarInfo() {
+        return infoRepository.findAll()
+                .flatMap(carInfo -> priceRepository.findByIdIn(carInfo.getPriceIds())
+                        .collectList()
+                        .map(prices -> {
+                            ArrayList<CarPrice> list = prices.stream()
+                                    .sorted(Comparator.comparing(CarPrice::getCreateTime))
+                                    .collect(Collectors.toCollection(ArrayList::new));
+                            carInfo.setPrices(list);
+                            return carInfo;
+                        }));
+    }
+}
---
+++ car-info-service/src/test/java/com/andysrv/carinfoservice/service/CarInfoServiceTest.java
@@ -0,0 +1,54 @@
+package com.andysrv.carinfoservice.service;
+
+import com.andysrv.carinfoservice.dto.VendorType;
+import com.andysrv.carinfoservice.entity.CarInfo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CarInfoServiceTest {
+
+    @Autowired
+    private CarInfoService carInfoService;
+
+    @Test
+    void saveOrUpdate() {
+        CarInfo carInfo = CarInfo.builder()
+                .title("2011 SuperCar sonic")
+                .year(2015)
+                .brand("BMW")
+                .price(1000000.0)
+                .queryTime(LocalDateTime.now())
+                .vendorType(VendorType.SHOU_SHI)
+                .build();
+        Mono<CarInfo> mono = carInfoService.saveOrUpdate(Mono.just(carInfo));
+        StepVerifier.create(mono)
+                .consumeNextWith(carInfo1 -> {
+                    assert carInfo1.getId() != null;
+                    assert carInfo1.getPriceIds() != null;
+                    assertEquals(carInfo.getTitle(), carInfo1.getTitle());
+                })
+                .verifyComplete();
+
+    }
+
+    @Test
+    void getAllCarInfo() {
+        Mono<List<CarInfo>> list$ = carInfoService.getAllCarInfo()
+                .collectList();
+        StepVerifier.create(list$)
+                .consumeNextWith(carInfos -> {
+                    String s = "";
+                })
+                .verifyComplete();
+
+    }
+}
