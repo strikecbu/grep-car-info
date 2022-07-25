4be8b30c62a978695327b67189d76bf936137345
strikecbu <strikecbu@gmail.com>
2022-07-25 22:19:18 +0800

    [Scrape] Remove legacy codes

--- web-scraping/src/main/java/com/andy/grepcarinfo/model/VendorType.java
+++ web-scraping/src/main/java/com/andy/grepcarinfo/model/VendorType.java
@@ -1,36 +1,13 @@
 package com.andy.grepcarinfo.model;
 
-import com.andy.grepcarinfo.consumer.ScrapeType;
-import com.andy.grepcarinfo.service.ConnectGrepDataService2000Impl;
-import com.andy.grepcarinfo.service.ConnectGrepDataServiceShouShiImpl;
 
 import java.util.Arrays;
 import java.util.Optional;
 
 public enum VendorType {
 
-    SHOU_SHI("https://sscars.com.tw/car/", ConnectGrepDataServiceShouShiImpl.class),
-    TWO_THOUSAND(
-            "https://www.2000car.tw/blog/post/317462829-%E6%A1%83%E5%9C%92%E4%B8%AD%E5%A3%A2%E6%97%A5%E6%A6%AE%E6%B1%BD%E8%BB%8A-%E5%85%A9%E5%8D%83%E4%B8%AD%E5%8F%A4%E8%BB%8A%E8%BB%8A%E6%BA%90%E4%B8%80%E8%A6%BD%E8%A1%A8/blog/post/96336596", ConnectGrepDataService2000Impl.class);
-
-    private String url;
-    private Class grepDateServiceClass;
-    VendorType(String url, Class grepDateServiceClass) {
-        this.url = url;
-        this.grepDateServiceClass = grepDateServiceClass;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public Class getGrepDateServiceClass() {
-        return grepDateServiceClass;
-    }
-
-    public void setUpdateDate() {
-
-    }
+    SHOU_SHI,
+    TWO_THOUSAND;
 
     public static Optional<VendorType> values(String value) {
         return Arrays.stream(VendorType.values())
