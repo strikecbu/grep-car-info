9c529b6378344494f753596831c931c87ddded32
strikecbu <strikecbu@gmail.com>
2022-07-27 23:50:53 +0800

    [CarInfo|Scrape] Create topic only on dev

--- web-scraping/src/main/java/com/andy/grepcarinfo/config/KafkaNewTopicConfig.java
+++ web-scraping/src/main/java/com/andy/grepcarinfo/config/KafkaNewTopicConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
-//@Profile("local")
+@Profile("dev")
 public class KafkaNewTopicConfig {
 
     @Value("${carinfo.kafka.producer.carInfoEvent.topic}")
